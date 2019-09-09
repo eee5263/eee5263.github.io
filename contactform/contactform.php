906eb037de5436f1ea6f0c5b3e8387834355d325
eee5263 <eee5263@gmail.com>
2019-09-09 17:42:33 +0900

    contactform php email added

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -8,7 +8,7 @@
 /***************** Configuration *****************/
 
   // Replace with your real receiving email address
-  $contact_email_to = "contact@example.com";
+  $contact_email_to = "eee5263@gmail.com";
 
   // Title prefixes
   $subject_title = "Contat Form Message:";
